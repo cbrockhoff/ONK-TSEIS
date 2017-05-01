c5317c0043faae58325d221684ab99da0e4a7d69
cbrockhoff <cbrockhoff1@gmail.com>
2017-05-02 00:28:02 +0200

    Added initial solution and test project.

---
+++ TSEIS/Test.Service/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> EchoValue(int id)
+        {
+            await Task.Delay(new Random().Next(20, 1000)); // heavy work
+            return new ObjectResult(id);
+        }
+    }
+}
